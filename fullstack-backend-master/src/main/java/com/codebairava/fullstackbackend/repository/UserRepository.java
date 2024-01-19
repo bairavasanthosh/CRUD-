10903e57b8e905f3f45aa0caf422a4be0ef9fbaf
bairavasanthosh <156896817+bairavasanthosh@users.noreply.github.com>
2024-01-19 04:47:56 -0800

    update

---
+++ fullstack-backend-master/src/main/java/com/codebairava/fullstackbackend/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.codebairava.fullstackbackend.repository;
+
+import com.codebairava.fullstackbackend.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User,Long> {
+}
